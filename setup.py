8b06cdac27eec63afd92fadd60a2b0fe548a03b0
Jezri <jezrikrinsky@gmail.com>
2017-04-24 06:00:48 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='koneroj',
+	version='0.0.3',
+	description='A base package for making a functional base of knowledge',
+	url='',
+	author='Jezri Krinsky',
+	author_email='',
+	license='',
+	packages=['koneroj'],
+	zip_safe=False)
+	
+	
