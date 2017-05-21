a70ea8065cbdc2b5ef2863416261c62d11e7bef0
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-21 07:26:10 +0200

    module name now functions

--- koneroj/module_name.py
+++ koneroj/module_name.py
@@ -1,10 +1,16 @@
 from string import*
-class module_name(object):
-	def __init__ (self, name):
-		self.desplay = name
-	def get_code(self):
-		temp =  self.desplay.lower()
-		temp = temp.replace(' ','_')
-		return temp
-        code = property(get_code) 
-
+def name():
+    return "Some name"
+print name()
+def code(name):
+    temp = name()
+    temp = temp.lower
+    return  temp.replace(' ','_')
+def desplay(name):
+    return name()
+print desplay(name)
+def code_name():
+    return code(name())
+def desplay_name():
+    return desplay(name)
+print desplay_name()
