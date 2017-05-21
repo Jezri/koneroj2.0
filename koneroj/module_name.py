79ba2d50972ff49f07cfa28de5e5fc9a985b3946
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-21 10:23:18 +0200

    undid name function

--- koneroj/module_name.py
+++ koneroj/module_name.py
@@ -1,11 +1,10 @@
 from string import*
-def name():
-    return "Some name"
-def code(name):
-    return name().lower().replace(' ','_') 
-def desplay(name):
-    return name()
-def code_name():
-    return code(name)
-def desplay_name():
-    return desplay(name)
+class module_name(object):
+    def __init__(self, name):
+         self.desplay = name
+    def get_code(self):
+        temp = self.desplay.lower()
+        temp = temp.replace(' ','_')
+        return temp
+    code = property(get_code)
+
