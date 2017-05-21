8005e004f4c0b7723caec0f911239cace56cd3e7
Jezri Krinsky <jezri@debian>
2017-05-21 07:06:56 +0200

    version 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='koneroj',
-	version='0.0.3',
+	version='0.0.4',
 	description='A base package for making a functional base of knowledge',
 	url='https://github.com/Jezri/koneroj.git',
 	author='Jezri Krinsky',
