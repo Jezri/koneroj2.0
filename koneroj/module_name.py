e867109163c6c881be70c27217b9766878e0f15f
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-21 08:19:53 +0200

    trying to rethink module parts

--- koneroj/module_name.py
+++ koneroj/module_name.py
@@ -1,16 +1,11 @@
 from string import*
 def name():
     return "Some name"
-print name()
 def code(name):
-    temp = name()
-    temp = temp.lower
-    return  temp.replace(' ','_')
+    return name().lower().replace(' ','_') 
 def desplay(name):
     return name()
-print desplay(name)
 def code_name():
-    return code(name())
+    return code(name)
 def desplay_name():
     return desplay(name)
-print desplay_name()
