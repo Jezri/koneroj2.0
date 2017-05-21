3ff7e6985d58e1c874d569313a0282f63e50cd8b
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-21 10:29:53 +0200

    fully back to non functional version

--- koneroj/module_part.py
+++ koneroj/module_part.py
@@ -1,40 +1,32 @@
-from module_name import * 
-def statment(name):
-    return desplay(),concepts(),theorems(),techniques() 
-def desplay():
-    return """"""
-def concepts():
-    return []
-def theorems():
-    return []
-def techniques():
-    return []
-print module_part(name)
-#class module_part(object):
-#	def __init__(self):
-#		self.desplay = ''
-#		self.concepts = []
-#		self.techniques = []
-#               self.theorems =[]
-#      
-#	def write_item(self, label):
-#		return (r'\begin{item}['+label+']'+ self.desplay + '\end{item}')
-  
-#	def write_section(self):
-#		temp = ''   
-#		if self.desplay != '': 
-#			temp += r'\subsection*{' + self.name.desplay + r'}'
-#			temp +=  '\n' +self.desplay
-#			return temp
-       
-#	def add_thoerem(self,theorem,text):
-#		self.desplay += r"""\overbrace{ """+ text+r"""}^\text{"""+ theorem.name.desplay +r"""}"""
-#		self.theorems += [dependency]
-  
-#	def add_concept(self,concept):
-#		self.concepts += [concept]
 
-#	def add_technique(self,technique,text):
-#		self.desplay += r"""\overbrace{ """+ text+r"""}^\text{"""+ technique.name.desplay +r"""}"""
-#		self.techniques += [technique]
+
+from module_name import * 
+class module_part(object):
+	def __init__(self,name):
+		self.name = module_name(name)
+		self.desplay = ''
+		self.concepts = []
+		self.techniques = []
+                self.theorems =[]
+      
+	def write_item(self, label):
+		return (r'\begin{item}['+label+']'+ self.desplay + '\end{item}')
+  
+	def write_section(self):
+		temp = ''   
+		if self.desplay != '': 
+			temp += r'\subsection*{' + self.name.desplay + r'}'
+			temp +=  '\n' +self.desplay
+			return temp
+       
+	def add_thoerem(self,theorem,text):
+		self.desplay += r"""\overbrace{ """+ text+r"""}^\text{"""+ theorem.name.desplay +r"""}"""
+		self.theorems += [dependency]
+  
+	def add_concept(self,concept):
+		self.concepts += [concept]
+
+	def add_technique(self,technique,text):
+		self.desplay += r"""\overbrace{ """+ text+r"""}^\text{"""+ technique.name.desplay +r"""}"""
+		self.techniques += [technique]
           
