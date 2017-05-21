178db38203f323cda9ec0340cd833e3efa633b4e
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-21 15:11:17 +0200

    minor thoerem working

--- koneroj/writable_module.py
+++ koneroj/writable_module.py
@@ -2,15 +2,16 @@ from module import *
 class writable_module(module):
     def __init__(self,name):
         module.__init__(self,name)
-        self.minor_modules = []
+        self.minor_modules = {}
     def get_preamble(self):
         temp = r"""
-            \documentclass{article}      
-            \usepackage{fancyhdr}
-            \usepackage{mathtools}
-            \pagestyle{fancy}
-            \begin{document}  
-            \fancyhead[L]{"""+self.name.desplay + r"""}
+\documentclass{article}      
+    \usepackage{fancyhdr}
+    \usepackage{mathtools}
+    \usepackage{amssymb}
+    \pagestyle{fancy}
+\begin{document}  
+     \fancyhead[L]{"""+self.name.desplay + r"""}
         """
         return temp
     preamble = property(get_preamble)   
@@ -21,11 +22,11 @@ class writable_module(module):
         f.write(self.preamble)
         f.write(self.write_module())
         f.write(self.write_extras())
-        for minor_module in self.minor_modules:
-            f.write(minor_module.write_module())
         f.write(self.write_techniques())
         f.write(self.write_theorems())
         f.write(self.write_concepts())
+        for minor_module in self.minor_modules:
+            f.write("\section*{" + minor_module+"}"+ self.minor_modules[minor_module].write_module())
         f.write(r'\end{document}')
         f.close
 		
